88bfddba0cf1a5a9f7ba5119fe8c3c7c72e9c58c
Camilo Rojas <camilo.rojas9723@gmail.com>
2023-04-02 15:43:16 -0500

    Proyecto de subida

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Camera_Upload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(260, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Captura";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 198);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(591, 406);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(177, 39);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(237, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(619, 616);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Captura";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Camera_Upload
+{
+    public partial class Form1 : Form
+    {
+        private bool Devices;
+        private FilterInfoCollection MyDevice;
+        private VideoCaptureDevice MyCam;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDevice();
+        }
+        public void CargaDevice()
+        {
+            MyDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (MyDevice.Count > 0)
+            {
+                Devices = true;
+                for (int i = 0; i < MyDevice.Count; i++)
+                {
+                    comboBox1.Items.Add(MyDevice[i].Name);
+                    comboBox1.Text = MyDevice[0].ToString();
+                }
+            }
+            else
+            {
+                Devices = false;
+            }
+        }
+        public void CloseCam()
+        {
+            if (MyCam != null && MyCam.IsRunning)
+            {
+                MyCam.SignalToStop();
+                MyCam = null;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CloseCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = MyDevice[i].MonikerString;
+            MyCam = new VideoCaptureDevice(NombreVideo);
+            MyCam.NewFrame += new NewFrameEventHandler(Captura);
+            MyCam.Start();
+        }
+
+        private void Captura(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = Imagen;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseCam();
+        }
+    }
+}
